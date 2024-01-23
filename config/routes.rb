d26dd266b8828b7453fa7135eea23da593fbd158
Timeea Samoila <timeeasamoila12@gmail.com>
2024-01-23 19:31:17 +0100

    tasks 1

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new'
 end
---
+++ db/migrate/20240123135631_add_default_to_completed_in_tasks.rb
@@ -0,0 +1,5 @@
+class AddDefaultToCompletedInTasks < ActiveRecord::Migration[7.1]
+  def change
+    change_column :tasks, :completed, :boolean, default: false
+  end
+end
